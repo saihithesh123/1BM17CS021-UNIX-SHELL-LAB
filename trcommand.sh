5327590fb1e29c0792e1a2e3e3cc38a3771f1a34
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2020-12-10 15:53:53 +0530

    Delete trcommand.sh

--- trcommand.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter the file name : " 
-read filename 
-if [ -f $filename ] 
-then 
-	echo " Total number of vowels in file is : "`cat $filename | tr -dc [aeiouAEIOU] | wc -c`
-else 
-	echo "Filename doesn't exist in directory "
-fi	
